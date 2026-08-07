54d24e3f9373bf2f108c9aefe1448deeee1a74de
mezas3238-hue <mezas3238@gmail.com>
2026-08-07 17:55:56 -0300

    QORE-SPECIALIZED-GOVERNANCE-001: export specialized flow

---
+++ src/qore/specialized_governance/__init__.py
@@ -0,0 +1,17 @@
+"""Cross-service governance for QORE specialized intelligence."""
+
+from qore.specialized_governance.decision_flow import (
+    SpecializedGovernanceError,
+    SpecializedGovernanceValidationError,
+    SpecializedServicesDecisionFlow,
+    SpecializedServicesDecisionFlowPlan,
+    SpecializedServicesDecisionFlowResult,
+)
+
+__all__ = [
+    "SpecializedGovernanceError",
+    "SpecializedGovernanceValidationError",
+    "SpecializedServicesDecisionFlow",
+    "SpecializedServicesDecisionFlowPlan",
+    "SpecializedServicesDecisionFlowResult",
+]
