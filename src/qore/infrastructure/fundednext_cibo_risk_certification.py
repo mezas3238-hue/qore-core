d480f04d6c7c229dd4759941d9404ef7b956e130
mezas3238-hue <mezas3238@gmail.com>
2026-09-16 00:09:05 -0300

    bind certification to current provider 3pct cap

--- src/qore/infrastructure/fundednext_cibo_risk_certification.py
+++ src/qore/infrastructure/fundednext_cibo_risk_certification.py
@@ -73,7 +73,7 @@ def build_certification(*, git_sha: str) -> dict[str, object]:
                 if SEPARATE_MAX_RISK_AT_ANY_TIME_FRACTION is None
                 else str(SEPARATE_MAX_RISK_AT_ANY_TIME_FRACTION)
             ),
-            "provider_three_percent_rule_used_by_risk": False,
+            "provider_three_percent_rule_used_by_risk": True,
         },
         "cibo": {
             "version": R315_CIBO_VERSION,
