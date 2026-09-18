6b1a0c56a5fd4955e9e51cc59dc15a636533d7ec
mezas3238-hue <mezas3238@gmail.com>
2026-09-18 03:07:25 -0300

    Include XAUUSD in FundedNext no-send probe

--- scripts/fundednext_mt5_no_send_probe.py
+++ scripts/fundednext_mt5_no_send_probe.py
@@ -11,7 +11,7 @@ from typing import Any
 
 mt5 = importlib.import_module("MetaTrader5")
 
-RETAINED_SYMBOLS = ("AUDJPY", "GBPUSD", "GBPJPY")
+RETAINED_SYMBOLS = ("AUDJPY", "GBPUSD", "GBPJPY", "XAUUSD")
 EXPECTED_SERVER = "FundedNext-Server"
 
 
