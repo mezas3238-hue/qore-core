ca4e8dd16c8274348b0da17786dd60b21c8ab560
mezas3238-hue <mezas3238@gmail.com>
2026-09-18 01:19:45 -0300

    Fix R30 unused import

--- src/qore/infrastructure/trader_lab/turtle_soup_xauusd_r30_nearest_dol_coverage_lab.py
+++ src/qore/infrastructure/trader_lab/turtle_soup_xauusd_r30_nearest_dol_coverage_lab.py
@@ -41,9 +41,6 @@ from qore.infrastructure.trader_lab import (
     turtle_soup_xauusd_r26_specialist_memory_brain as r26,
 )
 from qore.infrastructure.trader_lab import (
-    turtle_soup_xauusd_r29_coverage_expansion_lab as r29,
-)
-from qore.infrastructure.trader_lab import (
     turtle_soup_xauusd_specialist_cognitive_memory_v2 as v2,
 )
 from qore.infrastructure.trader_lab import (
