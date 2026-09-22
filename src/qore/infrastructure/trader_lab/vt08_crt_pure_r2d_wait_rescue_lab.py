5f70ca5c7e2640483d1be8a4596995a9dbf5bd2f
mezas3238-hue <mezas3238@gmail.com>
2026-09-22 19:29:57 -0400

    fix(vt08): clean R2-D rescue lab imports

--- src/qore/infrastructure/trader_lab/vt08_crt_pure_r2d_wait_rescue_lab.py
+++ src/qore/infrastructure/trader_lab/vt08_crt_pure_r2d_wait_rescue_lab.py
@@ -53,7 +53,6 @@ from qore.infrastructure.trader_lab.vt08_crt_pure_r2_model1_reference_lab import
     M15Bar,
     ParentCrt,
     ReferenceKind,
-    _fold,
     aggregate_complete_m15,
     build_parent_crts,
 )
