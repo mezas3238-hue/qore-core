cf37591f15bc3b57969e01260d0a5a31162e53e0
mezas3238-hue <mezas3238@gmail.com>
2026-09-23 20:39:09 -0400

    test(vt08): bind density funnel to truth corpus

---
+++ tests/infrastructure/trader_lab/test_vt08_cognitive_5m_density_funnel_audit_v1.py
@@ -0,0 +1,9 @@
+from qore.infrastructure.trader_lab.vt08_cognitive_5m_density_funnel_audit_v1 import (
+    SOURCE_HEAD,
+    SOURCE_RUN_ID,
+)
+
+
+def test_density_funnel_is_bound_to_same_truth_corpus() -> None:
+    assert SOURCE_RUN_ID == 35934924907
+    assert SOURCE_HEAD == "b2d33e1b4829d8b4afc76983decca8a99131403c"
